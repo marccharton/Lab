969c38e1d047219359f08a2bd46ae2c1edf0d697
Marc Charton <mcharton@altirc.com>
2015-02-17 14:08:32 +0100

    REELLE Correction du tuto AdrianMejia !! l'ancienne version etait vraiment noob ! :/

--- techno/angular.js/tuto/AdraianMejia/src/app.js
+++ techno/angular.js/tuto/AdraianMejia/src/app.js
@@ -1,24 +1,29 @@
 
+var app = angular.module('app', ['ngRoute']);
 
-angular.module('app', ['ngRoute'])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: '/todos.html',
-        controller: 'TodoController'
-      });
-  }]);
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/todos.html',
+      controller: 'TodoController'
+    });
+}]);
 
-
-  angular.module('app', [])
-  .controller('TodoController', ['$scope', function ($scope) {
-    $scope.todos = [
-      { title: 'Learn Javascript', completed: true },
-      { title: 'Learn Angular.js', completed: false },
-      { title: 'Love this tutorial', completed: true },
-      { title: 'Learn Javascript design patterns', completed: false },
-      { title: 'Build Node.js backend', completed: false },
-    ];
-  }]);
+app.controller('TodoController', ['$scope', function ($scope) {
+  $scope.todos = [
+    { title: 'Learn Javascript', completed: true },
+    { title: 'Learn Angular.js', completed: false },
+    { title: 'Love this tutorial', completed: true },
+    { title: 'Learn Javascript design patterns', completed: false },
+    { title: 'Build Node.js backend', completed: false },
+  ];
+}]);
 
 
+app.directive('hello', [function () {
+  return {
+    restrict: 'CEMA', // C: class, E: element, M: comments, A: attributes
+    replace: true, // replaces original content with template
+    template: '<span><br>Hello</span>'
+  }
+}]);
