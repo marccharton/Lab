fa653262bdba9f1139fc1175a42e2284e593380f
Marc Charton <mcharton@altirc.com>
2015-03-10 14:47:18 +0100

    appel à l'api pour historique, test angular (pas encore ça)

--- Historique Deezer/Deezer History/src/app.js
+++ Historique Deezer/Deezer History/src/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
+var history = require('./routes/history');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api', api);
+app.use('/history', history);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
