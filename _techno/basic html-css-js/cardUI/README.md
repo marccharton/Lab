0f1d5e8c2b6bd98e93a0b9f74d6772651c0de53f
kram47 <Mcharton@hotmail.fr>
2015-07-27 22:25:28 +0200

    Tuto Grafikart CardUI

---
+++ _techno/basic html-css-js/cardUI/README.md
@@ -0,0 +1,22 @@
+Portfolio Vincent
+==================
+
+
+
+Prérequis
+----------
+
+installation de node  
+installastion de git  
+
+Installation
+-------------
+
+	$> git clone [url_du_projet]  
+	$> npm install  
+	$> bower install  
+
+Lancement
+----------
+
+ouverture du fichier index.html  
---
+++ _techno/basic html-css-js/cardUI/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var compass = require('gulp-compass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+
+gulp.task('styles', function () {
+	var stream = gulp;
+
+	stream.src('style/scss/**/*.scss')
+	.pipe(compass({
+            sass: 'style/scss',
+            css: 'style/css'
+        }))
+	.pipe(autoprefixer())
+	.pipe(concat("style.css"))
+	.pipe(notify("Task 'styles' completed"))
+	.pipe(livereload());
+	return stream;
+});
+
+gulp.task('view', function () {
+	return gulp.src('*.html')
+	.pipe(livereload());
+});
+
+gulp.task('watch', function () {
+	livereload.listen();
+	gulp.watch("style/scss/**/*.scss", ["styles"]);
+	gulp.watch("*.html", ["view"]);
+	
+	gulp.watch(['gulpfile.js'], function () {
+        console.log('[WARNING] gulpfile.js was changed');
+    });
+});
+
+gulp.task('default', ['watch']);
